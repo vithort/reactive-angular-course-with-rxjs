245048b0dc9127e1c0ab9b8c5801e4f00669a105
Vithor Carvalho <vithort@gmail.com>
2021-04-17 09:37:09 -0300

    Consuming Observable-based services using the Angular async Pipe

---
+++ reactive-angular-course/src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Course } from "../model/course";
+
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>("/api/courses")
+    .pipe(
+      map(res => res['payload'])
+    );
+  }
+}
