2603090224a57198cecee2ca3755c602d9ca0790
Vithor Carvalho <vithort@gmail.com>
2021-04-17 17:10:21 -0300

    Reactive Angular - The Single Data Observable Pattern

--- reactive-angular-course/src/app/course/course.component.ts
+++ reactive-angular-course/src/app/course/course.component.ts
@@ -21,9 +21,21 @@ import {
   shareReplay,
   catchError,
 } from 'rxjs/operators';
-import { merge, fromEvent, Observable, concat, throwError } from 'rxjs';
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course',
@@ -31,8 +43,9 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit {
-  course$: Observable<Course>;
-  lessons$: Observable<Lesson[]>;
+  //course$: Observable<Course>;
+  //lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>;
 
   constructor(
     private route: ActivatedRoute,
@@ -41,7 +54,16 @@ export class CourseComponent implements OnInit {
 
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
-    this.course$ = this.coursesService.loadCourseById(courseId);
-    this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const course$ = this.coursesService.loadCourseById(courseId);
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
   }
 }
