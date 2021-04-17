7a75a2b56f192b146df544fd2febff18ad345eef
Vithor Carvalho <vithort@gmail.com>
2021-04-17 15:45:09 -0300

    Authentication Store - Step-By-Step Implementation

--- reactive-angular-course/src/app/services/auth.stores.ts
+++ reactive-angular-course/src/app/services/auth.stores.ts
@@ -1,18 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
 import { User } from '../model/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthStore {
-  user$: Observable<User>;
+  private subject = new BehaviorSubject<User>(null);
+  user$: Observable<User> = this.subject.asObservable();
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
 
-  login(email: string, password: string): Observable<User> {
-    return undefined;
+  constructor(private http: HttpClient) {
+    this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedin) => !loggedin));
   }
 
-  logout() {}
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post<User>('/api/login', { email, password })
+      .pipe(
+        tap((user) => this.subject.next(user)),
+        shareReplay()
+      );
+  }
+
+  logout() {
+    this.subject.next(null);
+  }
 }
