609b5012d70061ba24e2573ccadc63739912788b
Vithor Carvalho <vithort@gmail.com>
2021-04-17 17:29:25 -0300

    Refactoring an Angular Reactive Application to OnPush Change Detection

--- reactive-angular-course/src/app/courses-card-list/courses-card-list.component.ts
+++ reactive-angular-course/src/app/courses-card-list/courses-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { filter, tap } from 'rxjs/operators';
 import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
@@ -8,6 +8,7 @@ import { Course } from '../model/course';
   selector: 'courses-card-list',
   templateUrl: './courses-card-list.component.html',
   styleUrls: ['./courses-card-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoursesCardListComponent implements OnInit {
   @Input()
