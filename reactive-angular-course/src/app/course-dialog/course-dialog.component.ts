90935ae8f2e11e32f9cb6d842cf5f4e10fb3e252
Vithor Carvalho <vithort@gmail.com>
2021-04-17 14:23:44 -0300

    Local Error Handling in an Angular Material Dialog

--- reactive-angular-course/src/app/course-dialog/course-dialog.component.ts
+++ reactive-angular-course/src/app/course-dialog/course-dialog.component.ts
@@ -15,12 +15,13 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService],
+  providers: [LoadingService, MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
   form: FormGroup;
@@ -31,7 +32,8 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
     private coursesService: CoursesService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {
     this.course = course;
 
@@ -47,7 +49,16 @@ export class CourseDialogComponent implements AfterViewInit {
 
   save() {
     const changes = this.form.value;
-    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+    const saveCourse$ = this.coursesService
+      .saveCourse(this.course.id, changes)
+      .pipe(
+        catchError((err) => {
+          const message = 'Could not save course';
+          console.log(message, err);
+          this.messagesService.showErrors(message);
+          return throwError(err);
+        })
+      );
     this.loadingService
       .showLoaderUntilCompleted(saveCourse$)
       .subscribe((val) => {
