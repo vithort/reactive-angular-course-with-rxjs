93e037adc926b9e67eddc85683657fcae32d0683
Vithor Carvalho <vithort@gmail.com>
2021-04-17 11:31:38 -0300

    Loading Service Reactive API Design

--- reactive-angular-course/src/app/loading/laoding.service.ts
+++ reactive-angular-course/src/app/loading/laoding.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class LoadingService {
+  loading$: Observable<boolean>;
 
-  constructor() {}
+  showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+    return undefined;
+  }
+
+  loadingOn() {}
+
+  loadingOff() {}
 }
--- reactive-angular-course/src/app/loading/loading.component.ts
+++ reactive-angular-course/src/app/loading/loading.component.ts
@@ -8,7 +8,7 @@ import { LoadingService } from './laoding.service';
   styleUrls: ['./loading.component.css'],
 })
 export class LoadingComponent implements OnInit {
-  constructor(private loadingService: LoadingService) {}
+  constructor(public loadingService: LoadingService) {}
 
   ngOnInit() {}
 }
